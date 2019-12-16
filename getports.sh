40cdc6e8838fa4bffa45d70216b86c9938097efb
Nathhill92 <58561043+Nathhill92@users.noreply.github.com>
2019-12-16 03:43:16 -0600

    Update getports.sh

--- getports.sh
+++ getports.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-#This script shows open IPv4 ports on a system
-
+#This script shows open ports on a system
 netstat -nutl | grep ':' | awk '{print $4}' | awk -F ':' '{print $NF}' | sort -hu
-
-#IPv6 version:
-    #### netstat -nutl | grep ':' | awk '{print $4}' | awk -F ':' '{print $NF}'
